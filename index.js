6acc26197e329155eab61f310b1c50a4eeeb95d3
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-07 18:42:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+class Driver {
+  constructor(name,startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year) {
 
+    return year - this.startDate.getFullYear() - 1
+  }
+}
+
+class Route {
+  constructor(beginningLocation,endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation
+  }
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let vert = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+    let horizontal = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal))
+    return vert + horizontal
+  }
+
+  estimatedTime(peak) {
+    let min;
+    if (peak === true) {
+       min = this.blocksTravelled()/2
+    } else {
+       min = this.blocksTravelled()/3
+    }
+    return min
+  }
+}
